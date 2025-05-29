2e4d79db1da3dd42d2671f2793f5a7b8bff9adf0
Juan Quenga <110294740+JuanQuenga@users.noreply.github.com>
2025-05-29 13:25:24 -0400

    Add Google Fonts and update styles for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#8B0000] to-[#000000] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Welcome <span className="text-[hsl(0,100%,50%)]">Clifford</span>, your
-          new email pet.
+          Welcome{" "}
+          <span className="cookie text-[8rem] text-[hsl(0,100%,50%)]">
+            Clifford
+          </span>
+          , your new email pet.
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
